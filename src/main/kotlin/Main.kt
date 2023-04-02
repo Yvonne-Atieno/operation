8fc2f5857f987ef7af23f62f062fa865b25a6ff2
NjeriMarion <njeriwmarion@gmail.com>
2023-04-02 07:46:36 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,45 @@
+fun main() {
+    var num1 = 240
+    var num2 = 45
+    var addition = num1 + num2
+    println(addition)
+    println(num1 + num2 / 2)
+
+    var division = addition / 2
+    println(division)
+
+    var name = "yvonne"
+    var a = 3
+    var b = 2
+    println(a)
+    a = a + b
+
+  a=a*b
+  println(a)
+  a=a-b
+  println(a)
+
+    var age=23
+    println(age++)
+    println(++age)
+    println(a<b)
+    println(b<a)
+    println(b>a)
+    println(a>b)
+
+    println(a==b)
+    println(2==b)
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
